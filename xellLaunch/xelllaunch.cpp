52ad91669e2a7d4287ced175edb12b62f7d0f2c1
Mitchell Waite <mitchellwaite@users.noreply.github.com>
2025-10-11 21:59:45 -0400

    Initial Commit

---
+++ xellLaunch/xelllaunch.cpp
@@ -0,0 +1,203 @@
+//--------------------------------------------------------------------------------------
+//	start xell from a xex, required documented hv patch below to work
+//--------------------------------------------------------------------------------------
+#include <xtl.h>
+#include <stdio.h>
+#include "kernel.h"
+#include <stdint.h>
+
+#define NAND_XELL_OFFSET 0x95060
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	HRESULT __stdcall ObCreateSymbolicLink( STRING*, STRING*);
+	HRESULT __stdcall ObDeleteSymbolicLink( STRING* );
+
+	NTSYSAPI
+	DWORD
+	NTAPI
+	XexGetModuleHandle(
+		IN		PSZ moduleName,
+		IN OUT	PHANDLE hand
+		); 
+
+	// ie XexGetProcedureAddress(hand ,0x50, &addr) returns 0 on success
+	NTSYSAPI
+	DWORD
+	NTAPI
+	XexGetProcedureAddress(
+		IN		HANDLE hand,
+		IN		DWORD dwOrdinal,
+		IN		PVOID Address
+		);
+
+	NTSYSAPI
+	PVOID
+	NTAPI
+	MmGetPhysicalAddress(
+		IN		PVOID Address
+		);
+
+	NTSYSAPI
+	void
+	NTAPI
+	DbgPrint(
+		const char* s,
+		...
+		);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
+HRESULT Mount(PCHAR szDrive, PCHAR szDevice)
+{
+	CHAR szDestinationDrive[MAX_PATH];
+	sprintf_s(szDestinationDrive, MAX_PATH, "\\??\\%s", szDrive);
+	STRING DeviceName = MAKE_STRING(szDevice);
+	STRING LinkName = MAKE_STRING(szDestinationDrive);
+	ObDeleteSymbolicLink(&LinkName);
+	return (HRESULT)ObCreateSymbolicLink(&LinkName, &DeviceName);
+}
+
+// HvxGetVersion is the FreeBoot HV backdoor. op does the following:
+// 0 and 1 just return 1
+// 2 re-enables memory protection
+// 3 disables memory protection
+// 4 copies then jumps
+// 5 copies
+//
+// All we really need for XellLaunch is option 4, to copy data to HV space and jump to XeLL
+//
+#define HVX_MAGIC_NUMBER 0x72627472
+
+uint64_t __declspec(naked) HvxGetVersion(uint32_t magic, int op, uint64_t source, uint64_t dest, uint64_t length) {
+    __asm
+    {
+        li r0, 0x0
+        sc
+        blr
+    }
+}
+
+void HvxExecute(uint64_t address, void *code, size_t length)
+{
+	size_t physicalLength = 0x40000;
+
+    // allocate a buffer for our execute 
+    uint8_t *payload_buf = (uint8_t *)XPhysicalAlloc(physicalLength, MAXULONG_PTR, 0, PAGE_READWRITE);
+    uint64_t payload_addr = 0x8000000000000000 | (uint64_t)MmGetPhysicalAddress(payload_buf);
+    memcpy(payload_buf, code, length);
+    
+	// Call the FreeBoot backdoor
+	HvxGetVersion( HVX_MAGIC_NUMBER, 4, address, payload_addr, physicalLength );
+
+    XPhysicalFree(payload_buf);
+}
+
+static LPCWSTR buttons[1] = {L"Fuck!"};
+static MESSAGEBOX_RESULT result;
+static XOVERLAPPED overlapped;
+static void MessageBox(wchar_t *text)
+{
+    if (XShowMessageBoxUI(XUSER_INDEX_ANY, L"XellLaunch2 Error", text, 1, buttons, 0, XMB_ERRORICON, &result, &overlapped) == ERROR_IO_PENDING)
+    {
+        while (!XHasOverlappedIoCompleted(&overlapped))
+            Sleep(50);
+    }
+}
+
+char* devices[] = {
+	"\\Device\\Flash"
+	"\\Device\\Mass0\\",
+	"\\Device\\Mass1\\",
+	"\\Device\\Mass2\\",
+	"\\Device\\Harddisk0\\Partition1\\",
+	"\\Device\\Cdrom0\\",
+};
+char fileName[] = "xl:\\xell.bin";
+
+BYTE xelldata[0x40000];
+DWORD xellsize;
+
+void startXell(BOOL useNand)
+{
+//	UINT64 dest = 0x8000000013000000ULL; // xell-ll
+//	UINT64 dest = 0x800000001c000000ULL; // xell-1f
+	UINT64 dest = 0x800000001c040000ULL; // xell-2f
+	UINT64 src = 0x8000000000000000ULL;
+	UINT64 len = 0;
+
+	if(useNand)
+	{
+		//src = 0x80000200C8095060ULL
+		src = 0x80000200C8070000ULL;
+		len = len+0x10000;
+		DbgPrint("syscall - starting xell nand\n");
+		HvxExecute(dest, (void *)src, len);
+	}
+	else
+	{
+		/*PBYTE xell_buf = (PBYTE)XPhysicalAlloc(0x40000, MAXULONG_PTR, 0, MEM_LARGE_PAGES|PAGE_READWRITE|PAGE_NOCACHE);
+		ZeroMemory(xell_buf, 0x40000);
+		memcpy(xell_buf, xelldata, xellsize);
+		src = src+((DWORD)MmGetPhysicalAddress(xell_buf));
+		len = 0+((xellsize/4)& 0xFFFFFFFF);*/
+		DbgPrint("syscall - starting xell file\n");
+		HvxExecute(dest, (void *)xelldata, xellsize);
+	}
+}
+
+DWORD readFile(const char* path)
+{
+	DWORD read = 0;
+	HANDLE file = CreateFile(path, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	if(file == INVALID_HANDLE_VALUE)
+		return read;
+	ReadFile(file, xelldata, 0x40000, &read, NULL);
+	CloseHandle(file);
+	return read;
+}
+
+VOID __cdecl main()
+{
+	//MessageBox(L"Starting to load XeLL!");
+
+	/*
+	int i;
+ check for xell.bin on mass0, mass1, mass2, hdd then CD; if found load it
+	for(i = 0; i < 5; i++)
+	{
+		Mount("xl:", devices[i]);
+		xellsize = readFile("xl:\\xell.bin");
+		if(xellsize != 0)
+			startXell(FALSE);
+	}
+	*/
+
+	// if not check in the launched xex path
+	xellsize = readFile("GAME:\\xell-1f.bin");
+	if(xellsize != 0)
+		HvxExecute(0x800000001c000000, (void *)xelldata, xellsize);
+	
+	xellsize = readFile("GAME:\\xell-gggggg.bin");
+	if(xellsize != 0)
+		HvxExecute(0x800000001c000000, (void *)xelldata, xellsize);
+
+	xellsize = readFile("GAME:\\xell-2f.bin");
+	if(xellsize != 0)
+		HvxExecute(0x800000001c040000, (void *)xelldata, xellsize);
+
+	// If it's not beside the xex, or in the flashfs, try loading from NAND
+	// TODO gotta implement this...
+	// - is the NAND memory mapped?
+	// - Do i have to manually read the pages?
+
+	MessageBox(L"Couldn't find a suitable XeLL image to load!");
+
+	// all else fails (it'll crash first unless the patches are missing) just drop back to dash
+	XLaunchNewImage(XLAUNCH_KEYWORD_DEFAULT_APP, 0);
+}
